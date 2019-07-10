22ff4c3e51d6289d6b0ba2d41bd11c2bb334da54
Jouda Hidri <hidrijouda@gmail.com>
2019-07-10 21:22:37 +0200

    Implement drop-down for config param

--- src/main/java/org/jenkinsci/plugins/curfew/CurfewConfig.java
+++ src/main/java/org/jenkinsci/plugins/curfew/CurfewConfig.java
@@ -7,6 +7,8 @@ import org.kohsuke.stapler.StaplerRequest;
 
 import hudson.Extension;
 import hudson.model.Descriptor;
+import hudson.util.ListBoxModel;
+import hudson.util.ListBoxModel.Option;
 import jenkins.model.GlobalConfiguration;
 import net.sf.json.JSONObject;
 
@@ -28,7 +30,7 @@ public class CurfewConfig extends jenkins.model.GlobalPluginConfiguration {
 		@Inject
 		private transient CurfewGlobalVariable curfewVar;
 		
-		private String mondayBefore = "6:00"; // todo other days of the week, time_zone, timeout
+		private String mondayBefore = "16"; // todo other days of the week, time_zone, timeout
 		
 		public CurfewConfigDesc() {
 			load();
@@ -45,6 +47,36 @@ public class CurfewConfig extends jenkins.model.GlobalPluginConfiguration {
         	save();
             return false;
         }
+        
+    	
+    	public ListBoxModel doFillMondayBeforeItems(){
+    	    return new ListBoxModel(
+    	        new Option("None", "00", mondayBefore.equals("00")),
+    	        new Option("01:00", "01", mondayBefore.equals("01")),
+    	        new Option("02:00", "02", mondayBefore.equals("02")),
+    	        new Option("03:00", "03", mondayBefore.equals("03")),
+    	        new Option("04:00", "04", mondayBefore.equals("04")),
+    	        new Option("05:00", "05", mondayBefore.equals("05")),
+    	        new Option("06:00", "06", mondayBefore.equals("06")),
+    	        new Option("07:00", "07", mondayBefore.equals("07")),
+    	        new Option("08:00", "08", mondayBefore.equals("08")),
+    	        new Option("09:00", "09", mondayBefore.equals("09")),
+    	        new Option("10:00", "10", mondayBefore.equals("10")),
+    	        new Option("11:00", "11", mondayBefore.equals("11")),
+    	        new Option("12:00", "12", mondayBefore.equals("12")),
+    	        new Option("13:00", "13", mondayBefore.equals("13")),
+    	        new Option("14:00", "14", mondayBefore.equals("14")),
+    	        new Option("15:00", "15", mondayBefore.equals("15")),
+    	        new Option("16:00", "16", mondayBefore.equals("16")),
+    	        new Option("17:00", "17", mondayBefore.equals("17")),
+    	        new Option("18:00", "18", mondayBefore.equals("18")),
+    	        new Option("19:00", "19", mondayBefore.equals("19")),
+    	        new Option("20:00", "20", mondayBefore.equals("20")),
+    	        new Option("21:00", "21", mondayBefore.equals("21")),
+    	        new Option("22:00", "22", mondayBefore.equals("22")),
+    	        new Option("23:00", "23", mondayBefore.equals("23")),
+    	        new Option("All Day", "24", mondayBefore.equals("24")));
+    	}
 
 		public String getMondayBefore() {
 			return mondayBefore;
