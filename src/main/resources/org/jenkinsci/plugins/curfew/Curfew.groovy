487ea1a8a559fb913680503cfc9d3a7c590f73f8
Jouda Hidri <hidrijouda@gmail.com>
2019-07-26 22:53:45 +0200

    Display message when no approval is required

--- src/main/resources/org/jenkinsci/plugins/curfew/Curfew.groovy
+++ src/main/resources/org/jenkinsci/plugins/curfew/Curfew.groovy
@@ -24,44 +24,40 @@ public class Curfew implements Serializable {
 		}
 	}
 
-	def checkPoint () {
+	private def checkPoint () {
 
-		if(times != null) {
-			// throw error instead - implement this in #GlobalVariable
-
-			if (checkByDay(times.get("mondayBefore"), times.get("mondayAfter"), Calendar.MONDAY)
-			|| checkByDay(times.get("tuesdayBefore"), times.get("tuesdayAfter"), Calendar.TUESDAY)
-			|| checkByDay(times.get("wednesdayBefore"), times.get("wednesdayAfter"), Calendar.WEDNESDAY)
-			|| checkByDay(times.get("thursdayBefore"), times.get("thursdayAfter"), Calendar.THURSDAY)
-			|| checkByDay(times.get("fridayBefore"), times.get("fridayAfter"), Calendar.FRIDAY)
-			|| checkByDay(times.get("saturdayBefore"), times.get("saturdayAfter"), Calendar.SATURDAY)
-			|| checkByDay(times.get("sundayBefore"), times.get("sundayAfter"), Calendar.SUNDAY)) {
-				return true
-			}
-
+		if (checkByDay(times.get("mondayBefore"), times.get("mondayAfter"), Calendar.MONDAY)
+		|| checkByDay(times.get("tuesdayBefore"), times.get("tuesdayAfter"), Calendar.TUESDAY)
+		|| checkByDay(times.get("wednesdayBefore"), times.get("wednesdayAfter"), Calendar.WEDNESDAY)
+		|| checkByDay(times.get("thursdayBefore"), times.get("thursdayAfter"), Calendar.THURSDAY)
+		|| checkByDay(times.get("fridayBefore"), times.get("fridayAfter"), Calendar.FRIDAY)
+		|| checkByDay(times.get("saturdayBefore"), times.get("saturdayAfter"), Calendar.SATURDAY)
+		|| checkByDay(times.get("sundayBefore"), times.get("sundayAfter"), Calendar.SUNDAY)) {
+			return true
 		}
 
-		return false
+		script.echo "No approval is required ..."
 
+		return false
 	}
 
-	def checkByDay(String minHour, String maxHour, int theDay) {
+	private def checkByDay(String minHour, String maxHour, int theDay) {
 
 		if (minHour == null || maxHour == null) {
 			return false
 		}
 
 		def date = new Date()
-		int today = date[Calendar.DAY_OF_WEEK] // todo with time zone
-		def hour = date.format('HH', TimeZone.getTimeZone(times.get("timeZone"))) // check null
+		int today = date[Calendar.DAY_OF_WEEK]
+		def tz = TimeZone.getTimeZone(times.get("timeZone"))
+		def hour = date.format('HH', tz).toInteger()
 
-		boolean tooEarly = hour.toInteger() < minHour.toInteger()
-		boolean tooLate = hour.toInteger() >= maxHour.toInteger()
+		boolean tooEarly = hour < minHour.toInteger()
+		boolean tooLate = hour >= maxHour.toInteger()
 
 		boolean tooEarlyOrTooLate = tooEarly || tooLate
 		boolean todayIsTheDay = today == theDay
 
 		return ( todayIsTheDay && tooEarlyOrTooLate )
 	}
-
 }
